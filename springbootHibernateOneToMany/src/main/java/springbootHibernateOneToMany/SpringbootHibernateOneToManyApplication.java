6355e82dc511c3892ee4c76462cda49c49f9c985
Jelena Mitrovic <jelenamitrovic980@gmail.com>
2020-07-17 15:32:21 +0200

    create project

---
+++ springbootHibernateOneToMany/src/main/java/springbootHibernateOneToMany/SpringbootHibernateOneToManyApplication.java
@@ -0,0 +1,13 @@
+package springbootHibernateOneToMany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateOneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHibernateOneToManyApplication.class, args);
+	}
+
+}
---
+++ springbootHibernateOneToMany/src/test/java/springbootHibernateOneToMany/SpringbootHibernateOneToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootHibernateOneToMany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHibernateOneToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
